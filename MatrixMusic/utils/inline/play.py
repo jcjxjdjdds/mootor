3d29053fb72c0429d43a6d8194145fc5fb245bd2
jcjxjdjdds <104429826+jcjxjdjdds@users.noreply.github.com>
2024-02-25 11:50:58 +0200

    Update play.py

--- MatrixMusic/utils/inline/play.py
+++ MatrixMusic/utils/inline/play.py
@@ -17,7 +17,7 @@ def track_markup(_, videoid, user_id, channel, fplay):
                 callback_data=f"MusicStream {videoid}|{user_id}|v|{channel}|{fplay}",
             ),
         ],
-        [InlineKeyboardButton(text=_["MATRIX_BUTTON"], url=f"https://t.me/VIP_ALBEAR")],
+        [InlineKeyboardButton(text=_["MATRIX_BUTTON"], url=f"https://t.me/S_MA4")],
         [
             InlineKeyboardButton(
                 text=_["CLOSE_BUTTON"],
@@ -67,7 +67,7 @@ def stream_markup_timer(_, chat_id, played, dur):
                 callback_data="GetTimer",
             )
         ],
-        [InlineKeyboardButton(text=_["MATRIX_BUTTON"], url=f"https://t.me/VIP_ALBEAR")],
+        [InlineKeyboardButton(text=_["MATRIX_BUTTON"], url=f"https://t.me/S_MA4")],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
     ]
     return buttons
@@ -119,7 +119,7 @@ def livestream_markup(_, videoid, user_id, mode, channel, fplay):
                 callback_data=f"LiveStream {videoid}|{user_id}|{mode}|{channel}|{fplay}",
             ),
         ],
-        [InlineKeyboardButton(text=_["MATRIX_BUTTON"], url=f"https://t.me/VIP_ALBEAR")],
+        [InlineKeyboardButton(text=_["MATRIX_BUTTON"], url=f"https://t.me/S_MA4")],
         [
             InlineKeyboardButton(
                 text=_["CLOSE_BUTTON"],
