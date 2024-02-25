eae870fa50164338da9ec4b44fc23c8878af8a94
jcjxjdjdds <104429826+jcjxjdjdds@users.noreply.github.com>
2024-02-25 12:26:17 +0200

    Update __main__.py

--- MatrixMusic/__main__.py
+++ MatrixMusic/__main__.py
@@ -5,11 +5,11 @@ from pyrogram import idle
 from pytgcalls.exceptions import NoActiveGroupCall
 
 import config
-from AlbearMusic import LOGGER, app, userbot
-from AlbearMusic.core.call import Zelzaly
-from AlbearMusic.misc import sudo
-from AlbearMusic.plugins import ALL_MODULES
-from AlbearMusic.utils.database import get_banned_users, get_gbanned
+from MatrixMusic import LOGGER, app, userbot
+from MatrixMusic.core.call import Zelzaly
+from MatrixMusic.misc import sudo
+from MatrixMusic.plugins import ALL_MODULES
+from MatrixMusic.utils.database import get_banned_users, get_gbanned
 from config import BANNED_USERS
 
 
@@ -35,25 +35,25 @@ async def init():
         pass
     await app.start()
     for all_module in ALL_MODULES:
-        importlib.import_module("AlbearMusic.plugins" + all_module)
-    LOGGER("ميــوزك البير").info("تم تحميل الاضافات ...✓")
+        importlib.import_module("MatrixMusic.plugins" + all_module)
+    LOGGER("ميــوزك ماتـركس").info("تم تحميل الاضافات ...✓")
     await userbot.start()
     await Zelzaly.start()
     try:
         await Zelzaly.stream_call("https://telegra.ph/file/29f784eb49d230ab62e9e.mp4")
     except NoActiveGroupCall:
-        LOGGER("ميــوزك البير").info(
+        LOGGER("ميــوزك ماتـركس").info(
             "خطأ .. قم بفتح المكالمة في مجموعة السجل الخاصه بك\n\nجارِ ايقاف بوت الميوزك . . ."
         )
         exit()
     except:
         pass
     await Zelzaly.decorators()
-    LOGGER("ميــوزك البير").info("AlbearThon")
+    LOGGER("ميــوزك ماتـركس").info("MatrixThon")
     await idle()
     await app.stop()
     await userbot.stop()
-    LOGGER("ميــوزك البير").info("جارِ ايقاف بوت الميوزك . . .")
+    LOGGER("ميــوزك ماتـركس").info("جارِ ايقاف بوت الميوزك . . .")
 
 
 if __name__ == "__main__":
