092c063448348060086853a8c40f16f017700689
jcjxjdjdds <104429826+jcjxjdjdds@users.noreply.github.com>
2024-02-25 11:09:09 +0200

    Add files via upload

---
+++ MatrixMusic/plugins/play/Devs.py
@@ -0,0 +1,122 @@
+import asyncio
+import os
+import time
+import requests
+from pyrogram import filters
+import random
+from pyrogram import Client
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, ReplyKeyboardMarkup
+from strings.filters import command
+from MatrixMusic import app
+from random import  choice, randint
+
+def get_file_id(msg: Message):
+    if msg.media:
+        for message_type in (
+            "photo",
+            "animation",
+            "audio",
+            "document",
+            "video",
+            "video_note",
+            "voice",
+            # "contact",
+            # "dice",
+            # "poll",
+            # "location",
+            # "venue",
+            "sticker",
+        ):
+            obj = getattr(msg, message_type)
+            if obj:
+                setattr(obj, "message_type", message_type)
+                return obj
+
+@app.on_message(
+    command(["المطور","البير","امير","مطور السورس","مبرمج السورس"])
+    & filters.group
+  
+)
+async def yas(client, message):
+    usr = await client.get_chat("O7_05")
+    name = usr.first_name
+    photo = await app.download_media(usr.photo.big_file_id)
+    await message.reply_photo(photo,       caption=f"– – – – – – – – – – – – – – – – – –\n↯︙𝖣𝖾𝗏 ↬ ⦗ {name} ⦘\n↯︙𝖴𝗌𝖤𝗋 ↬ ⦗ @{usr.username} ⦘\n↯︙𝖨𝖣 ↬ ⦗ {usr.id} ⦘\n↯︙𝖡𝗂𝖮 ↬ ⦗ {usr.bio} ⦘\n– – – – – – – – – – – – – – – – – –",  
+    reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        name, url=f"https://t.me/{usr.username}")
+                ],
+            ]
+        ),
+    )
+    
+@app.on_message(
+    command(["المطور مارو","مارو","المبرمج"])
+    & filters.group
+  
+)
+async def yas(client, message):
+    usr = await client.get_chat("O7_05")
+    name = usr.first_name
+    photo = await app.download_media(usr.photo.big_file_id)
+    await message.reply_photo(photo,       caption=f"– – – – – – – – – – – – – – – – – –\n↯︙𝖣𝖾𝗏 ↬ ⦗ {name} ⦘\n↯︙𝖴𝗌𝖤𝗋 ↬ ⦗ @{usr.username} ⦘\n↯︙𝖨𝖣 ↬ ⦗ {usr.id} ⦘\n↯︙𝖡𝗂𝖮 ↬ ⦗ {usr.bio} ⦘\n– – – – – – – – – – – – – – – – – –",  
+    reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        name, url=f"https://t.me/{usr.username}")
+                ],
+            ]
+        ),
+    )
+    
+@app.on_message(
+    command(["مطورين","مطورين السورس","المطورين","سورس","السورس"])
+  
+)
+async def huhh(client: Client, message: Message):
+    await message.reply_photo(
+        photo=f"https://telegra.ph/file/e3cfca6f4e88d85e2d357.jpg",
+        caption=f"""↯︙اهلا بك عزيزي {message.from_user.mention}\n↯︙مطورين سورس البير ميوزك""",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "‹ : 𝗗𝗘𝗩 𝗔𝗹𝗯𝗲𝗮𝗿 : ›", url=f"https://t.me/O7_05"), 
+                 ],[
+                    InlineKeyboardButton(
+                        "‹ : 𝗗𝗘𝗩 𝗔𝗹𝗯𝗲𝗮𝗿 : ›", url=f"https://t.me/O7_05"),
+                ],[
+                    InlineKeyboardButton(
+                        "‹ : 𝗦𝗢𝗨𝗥𝗖𝗘 𝗔𝗹𝗯𝗲𝗮𝗿 : ›", url=f"https://t.me/VIP_ALBEAR"),
+                ],
+
+            ]
+
+        ),
+
+    )
+
+@app.on_message(command(["تخ"]) & filters.group)
+async def huhh(client, message):
+    to_id = int(ahmed.split("to")[-1].split("in")[0])
+    from_id = int(ahmed.split("ahmed")[-1].split("to")[0])
+    in_id = int(caption.split("in")[-1])
+    to_url = f"tg://openmessage?user_id={to_id}"
+    from_url = f"tg://openmessage?user_id={from_id}"
+    ahmed = message.text
+    await message.reply_animation(
+        animation=f"https://telegra.ph/file/5a18fe591860a8a98f39f.mp4",
+        caption=f"""↯︙قتل ↫ ⦗ {app.get_chat(to_id).first_name}]({to_url}) ⦘\nالضحيه دا 😢 ↫ ⦗ [{app.get_chat(from_id).first_name}]({from_url}) ⦘\nانا لله وانـا اليـه راجعـون 😢😢""",
+    )
+    reply_markup=InlineKeyboardMarkup(
+
+       [
+           [
+               InlineKeyboardButton(
+                   "‹ : Albear 𝖳𝖾𝖠𝗆 : ›", url=f"https://t.me/VIP_ALBEAR"),
+           ],
+       ]
+    ),
