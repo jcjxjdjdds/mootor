f58161bf263e2b185489825c047eecf5a3a744b9
jcjxjdjdds <104429826+jcjxjdjdds@users.noreply.github.com>
2024-02-25 12:06:34 +0200

    Update config.py

--- config.py
+++ config.py
@@ -36,7 +36,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/DEVALPOP/BOP",
+    "https://github.com/jcjxjdjdds/mootor",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
